616df94e500efc212a2d1b4030ac574f7a587155
Hayden Main <hayden.main2@gmail.com>
2019-02-25 15:44:16 -0500

    finalized basic music functions

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 	let aud = document.querySelector('audio'),
 		play = document.querySelector('#playButton'),
 		pause = document.querySelector('#pauseButton'),
-		rewind = document.querySelector('#rewindButton');
+		rewind = document.querySelector('#rewindButton'),
+		tracks = document.querySelectorAll('.trackHolder');
 
 	//functions for buttons
 	function playAudio() {
@@ -19,10 +20,20 @@
 		aud.currentTime = 0;
 	}
 
+	function swapSource() {
+		let currentTrack = this.dataset.currenttrack;
+
+		aud.src = `audio/${currentTrack}`;
+		aud.load();
+		aud.play();
+	}
+
 	//event handling
 	play.addEventListener("click", playAudio);
 	pause.addEventListener("click", pauseAudio);
 	rewind.addEventListener("click", rewindAudio);
+
+	tracks.forEach(track => track.addEventListener("click", swapSource));
 
 })();
 
