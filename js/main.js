622137cea85955b06d9c727640bdbd64961ef565
Hayden Main <hayden.main2@gmail.com>
2019-02-25 15:00:11 -0500

    adding all files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(() => {
+
+	//variable stack
+	let aud = document.querySelector('audio'),
+		play = document.querySelector('#playButton'),
+		pause = document.querySelector('#pauseButton'),
+		rewind = document.querySelector('#rewindButton');
+
+	//functions for buttons
+	function playAudio() {
+		aud.play();
+	}
+
+	function pauseAudio() {
+		aud.pause(true);
+	}
+
+	function rewindAudio() {
+		aud.currentTime = 0;
+	}
+
+	//event handling
+	play.addEventListener("click", playAudio);
+	pause.addEventListener("click", pauseAudio);
+	rewind.addEventListener("click", rewindAudio);
+
+})();
